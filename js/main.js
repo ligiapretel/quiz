e91bf42941c0aedfcd88855bc86dea08011cf41a
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2024-03-17 15:50:56 -0300

    feat: Exibindo questão e alternativas do arquivo json

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("Entrei aqui");
+import questionsData from "../data/questions.json" assert { type: "json" };
+
+const question = document.querySelector(".question");
+const answersContainer = document.querySelector(".answers-container");
+
+let questionIndex = 0;
+
+function showQuestion () {
+    const currentQuestion = questionsData[questionIndex];
+    question.innerHTML = currentQuestion.title;
+
+    currentQuestion.answers.forEach(item=>{
+        const span = document.createElement("span");
+        
+        span.classList.add("alternative");
+        
+        span.innerHTML = item.alternative;
+        
+        answersContainer.appendChild(span);
+    })
+}
+
+showQuestion();
