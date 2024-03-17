c216d6145e22263677c59e72894f974fd9f2fbae
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2024-03-17 17:12:06 -0300

    feat: Criando função que exibe se a resposta está correta ou incorreta

--- js/main.js
+++ js/main.js
@@ -2,21 +2,45 @@ import questionsData from "../data/questions.json" assert { type: "json" };
 
 const question = document.querySelector(".question");
 const answersContainer = document.querySelector(".answers-container");
+const feedback = document.querySelector(".feedback");
 
 let questionIndex = 0;
+
+function showFeedback (event) {
+    let isCorrectAnswer = event.target.getAttribute("data-correct");
+
+    if (isCorrectAnswer === "false") {
+        feedback.classList.remove("correct");
+        feedback.classList.add("incorrect");
+        feedback.textContent = "Resposta incorreta";
+        return false;
+    }
+
+    feedback.classList.remove("incorrect");
+    feedback.classList.add("correct");
+    feedback.textContent = "Resposta correta";
+    return true;
+    
+}
 
 function showQuestion () {
     const currentQuestion = questionsData[questionIndex];
     question.innerHTML = currentQuestion.title;
 
-    currentQuestion.answers.forEach(item=>{
+    currentQuestion.answers.forEach(item => {
         const span = document.createElement("span");
         
         span.classList.add("alternative");
+
+        span.setAttribute("data-correct",item.isCorrect);
         
         span.innerHTML = item.alternative;
         
         answersContainer.appendChild(span);
+
+        const alternatives = document.querySelectorAll(".alternative");
+
+        alternatives.forEach(item => item.addEventListener("click", showFeedback));
     })
 }
 
