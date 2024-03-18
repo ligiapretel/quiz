f458df493bf1e6c0ecfb2364732bdd6b905dd281
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2024-03-17 22:10:36 -0300

    feat: Calculando ponto referente à resposta correta

--- js/main.js
+++ js/main.js
@@ -3,24 +3,45 @@ import questionsData from "../data/questions.json" assert { type: "json" };
 const question = document.querySelector(".question");
 const answersContainer = document.querySelector(".answers-container");
 const feedback = document.querySelector(".feedback");
+const totalPoints = document.querySelector(".points");
 
 let questionIndex = 0;
+let score = 0;
+let questionAnswered = false;
 
 function showFeedback (event) {
     let isCorrectAnswer = event.target.getAttribute("data-correct");
-
+    
     if (isCorrectAnswer === "false") {
         feedback.classList.remove("correct");
         feedback.classList.add("incorrect");
         feedback.textContent = "Resposta incorreta";
+        
         return false;
     }
-
+    
     feedback.classList.remove("incorrect");
     feedback.classList.add("correct");
     feedback.textContent = "Resposta correta";
+    
+    let rightAnswer = true;
+    showScore(rightAnswer, questionAnswered);
+    questionAnswered = true;
+    
     return true;
     
+}
+
+function showScore (isCorrect, questionAnswered) {
+
+    const pointsEarned = 1;
+
+    if (isCorrect && !questionAnswered) {
+        score = score + pointsEarned;
+        totalPoints.textContent = `${score} ponto(s)`;
+        return true;
+    }
+    return totalPoints.textContent = `${score} ponto(s)`;
 }
 
 function showQuestion () {
@@ -45,3 +66,4 @@ function showQuestion () {
 }
 
 showQuestion();
+showScore(score);
